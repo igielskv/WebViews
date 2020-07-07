4f5d0988309615b0d37b8465022e11181fa20f7a
Manoli <igielskv@icloud.com>
2020-07-07 10:15:53 +0200

    Initial Commit

---
+++ WebViews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WebViews
+//
+//  Created by Manoli on 07/07/2020.
+//  Copyright © 2020 macForce.one. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
